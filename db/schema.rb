fda6e9657dff071d6d62caa6d02a3dbde49080e0
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-16 10:35:40 -0600

    migrate product and categories to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_16_022153) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_16_161913) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -37,6 +37,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_022153) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "username"
@@ -49,6 +55,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_022153) do
     t.index ["province_id"], name: "index_customers_on_province_id"
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_product_categories_on_category_id"
+    t.index ["product_id"], name: "index_product_categories_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "provinces", force: :cascade do |t|
     t.string "name"
     t.decimal "GST"
@@ -59,4 +82,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_16_022153) do
   end
 
   add_foreign_key "customers", "provinces"
+  add_foreign_key "product_categories", "categories"
+  add_foreign_key "product_categories", "products"
 end
