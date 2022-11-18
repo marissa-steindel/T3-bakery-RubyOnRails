50cdb0e7a0a61c337c519d61dc8ea6f58137629e
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-18 17:17:45 -0600

    add many to many association between product and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+  # associations
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+
   # required for active storage
   has_one_attached :image
+  accepts_nested_attributes_for :product_categories, allow_destroy: true
 end
