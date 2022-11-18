2588b5a21556841f300641fa9733d63b7afc7fc4
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-18 12:10:12 -0600

    whitelist "image" attribute, customize active admin form to include file upload input field

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Product do
-  permit_params :name, :description, :price
+  # whitelist desired model attributes
+  permit_params :name, :description, :price, :image
+
+  # add image upload field to form - this is from the formtastic gem
+  form do |f|
+    # shows errors on :base - (validation errors)
+    f.semantic_errors
+    # builds an input field for every attribute
+    f.inputs
+    # adds the 'Submit' and 'Cancel' buttons
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
