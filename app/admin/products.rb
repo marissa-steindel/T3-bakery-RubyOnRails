6728aa3c97fadad1edaa958bf326d5acf438cbb4
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-18 16:03:40 -0600

    customize the index table to display image column

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -4,14 +4,22 @@ ActiveAdmin.register Product do
 
   # add image upload field to form - this is from the formtastic gem
   form do |f|
-    # shows errors on :base - (validation errors)
-    f.semantic_errors
-    # builds an input field for every attribute
-    f.inputs
-    # adds the 'Submit' and 'Cancel' buttons
+    f.semantic_errors # shows errors on :base - (validation errors)
+    f.inputs # builds an input field for every attribute
     f.inputs do
       f.input :image, as: :file
     end
-    f.actions
+    f.actions# adds the 'Submit' and 'Cancel' buttons
   end
+
+  # display image in index
+  index do
+    selectable_column
+    column :name
+    column :image
+    column :price
+    column :description
+    actions
+  end
+
 end
