f98579d2db01cf6011986ba722084e348a88e600
Marissa Steindel <marissa.steindel@gmail.com>
2022-11-21 11:45:56 -0600

    migrate Page model to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_18_011332) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_174447) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -81,6 +81,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_18_011332) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["province_id"], name: "index_customers_on_province_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "product_categories", force: :cascade do |t|
