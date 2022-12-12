316e4984d7b038eb16812ccc000e905b2c38f524
Marissa Steindel <marissa.steindel@gmail.com>
2022-12-12 16:39:45 -0600

    reorganize seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,15 @@
 require "csv"
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-
-Customer.destroy_all
-Province.destroy_all
-Product.destroy_all
-Category.destroy_all
+# Destroy all database content
+# Customer.destroy_all
+# Province.destroy_all
+# Product.destroy_all
+# Category.destroy_all
 # AdminUser.destroy_all
+
+
+# SEED ACTIVE ADMIN
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
 # if AdminUser.count.nil? || AdminUser.count == 0
 #   AdminUser.create(
@@ -15,11 +18,12 @@ Category.destroy_all
 #     )
 # end
 
+# SEED SALES TAX
 # # salestax_csv_file = Rails.root.join("db/salestax.csv")
 # # salestax_csv_data = File.read(csv_file)
 # # salestax_csv_data = CSV.parse(csv_data, headers: true)
 
-salestax_csv_data =  CSV.parse(File.read(Rails.root.join("db/salestax.csv")), headers: true)
+# salestax_csv_data =  CSV.parse(File.read(Rails.root.join("db/salestax.csv")), headers: true)
 
 # salestax_csv_data.each do |row|
 #     puts   row["province"]
@@ -29,54 +33,70 @@ salestax_csv_data =  CSV.parse(File.read(Rails.root.join("db/salestax.csv")), he
 #     puts    row["HST"]
 # end
 
-salestax_csv_data.each do |row|
-  province = Province.create(
-    name:   row["province"],
-    code:   row["code"],
-    PST:    row["PST"],
-    GST:    row["GST"],
-    HST:    row["HST"]
+# SEED customers and province simultaneously
+# salestax_csv_data.each do |row|
+#   province = Province.create(
+#     name:   row["province"],
+#     code:   row["code"],
+#     PST:    row["PST"],
+#     GST:    row["GST"],
+#     HST:    row["HST"]
+#   )
+#   rand(1..3).times do
+#     customer = Customer.create(
+#       name: Faker::Name.name,
+#       address: Faker::Address.street_address,
+#       username: Faker::Lorem.word,
+#       password: Faker::Internet.password,
+#       province: province
+#     )
+#     puts "#{customer.name} from #{province.name} created"
+#   end
+#   # puts seed.inspect
+# end
+
+# Province.all.each do |prov|
+#   puts "Name: #{prov.name}"
+#   puts "GST: #{prov.GST}"
+#   puts "HST: #{prov.HST}"
+#   puts "PST: #{prov.PST}"
+#   puts
+# end
+
+# SEED categories
+# categories = ["loaf", "sweet", "savoury", "pastry", "cookie", "cake", "muffin", "bun", "flatbread"]
+
+# categories.each do |p|
+#   new_cat = Category.create(name: p)
+# end
+
+# SEED PRODUCTS
+# # product_csv_file = Rails.root.join("db/products.csv")
+# # product_csv_data = File.read(csv_file)
+# # product_csv_data = CSV.parse(csv_data, headers: true)
+# product_csv_data =  CSV.parse(File.read(Rails.root.join("db/products.csv")), headers: true)
+
+# product_csv_data.each do |p|
+#     # puts p["name"]
+#     # puts p["price"]
+#     # puts p["description"]
+
+#   new_prod = Product.create(
+#     name: p["name"],
+#     price: p["price"].to_i,
+#     description: p["description"]
+#     category: "loaf"
+#   )
+# end
+
+# SEED ORDERS
+
+status = ["pending","paid","shipped"]
+20.times do
+  Order.create(
+    status: status[rand(0..2)]
   )
-  rand(1..3).times do
-    customer = Customer.create(
-      name: Faker::Name.name,
-      address: Faker::Address.street_address,
-      username: Faker::Lorem.word,
-      password: Faker::Internet.password,
-      province: province
-    )
-    puts "#{customer.name} from #{province.name} created"
-  end
-  # puts seed.inspect
-end
 
-Province.all.each do |prov|
-  puts "Name: #{prov.name}"
-  puts "GST: #{prov.GST}"
-  puts "HST: #{prov.HST}"
-  puts "PST: #{prov.PST}"
-  puts
-end
-
-
-product_csv_data =  CSV.parse(File.read(Rails.root.join("db/products.csv")), headers: true)
-
-product_csv_data.each do |p|
-    # puts p["name"]
-    # puts p["price"]
-    # puts p["description"]
-
-  new_prod = Product.create(
-    name: p["name"],
-    price: p["price"].to_i,
-    description: p["description"]
-  )
-end
-
-categories = ["loaf", "sweet", "savoury", "pastry", "cookie", "cake", "muffin", "bun"]
-
-categories.each do |p|
-  new_cat = Category.create(name: p)
 end
 
 
@@ -101,5 +121,3 @@ end
 #   #     end
 #   #   end
 #   # end
-
-# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
