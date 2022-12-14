a264d6552af5f1ea3e3b9cafde08dd56d99e4e49
Marissa Steindel <marissa.steindel@gmail.com>
2022-12-14 13:06:13 -0600

    document RESTFUL routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
-  root "pages#about"
+  root "products#index"
 
   get "customer/SignUp", to: "customers#add_customer", as: "add_customer"
 
   get "about", to: "pages#about"
   get "contact", to: "pages#contact"
+  get "login", to: "customers#login", as: "login"
 
   get "checkout", to: "checkout#index", as: "checkout"
+  get "checkout/apply_tax", to: "checkout#determine_tax", as: "apply_tax"
   get "checkout/province/:id", to: "checkout#determine_tax", as: "determine_tax"
 
 
@@ -40,6 +42,19 @@ Rails.application.routes.draw do
   resources :provinces
   resources :categories
   resources :orders
+
+  # GET	      /photos	          photos#index	  display a list of all photos
+  # POST	    /photos	          photos#create	  create a new photo
+  # GET	      /photos/new	      photos#new	    return an HTML form for creating a new photo
+  # GET	      /photos/:id	      photos#show	    display a specific photo
+  # PATCH/PUT	/photos/:id	      photos#update	  update a specific photo
+  # DELETE	  /photos/:id	      photos#destroy	delete a specific photo
+  # GET	      /photos/:id/edit	photos#edit	    return an HTML form for editing a photo
+
+  # photos_path           returns /photos
+  # new_photo_path        returns /photos/new
+  # edit_photo_path(:id)  returns /photos/:id/edit (edit_photo_path(10) returns /photos/10/edit)
+  # photo_path(:id)       returns /photos/:id (photo_path(10) returns /photos/10)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
